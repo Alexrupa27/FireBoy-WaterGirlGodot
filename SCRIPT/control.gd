d1009725d9ab7857b0edc04fe5f20d51ac1fc4a2
YasinEled <yasinelede@gmail.com>
2024-11-15 15:36:44 +0100

    exit

---
+++ SCRIPT/control.gd
@@ -0,0 +1,12 @@
+extends Control 
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_instruccions_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
