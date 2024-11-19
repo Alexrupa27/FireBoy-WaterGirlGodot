9f64fc69bfb5b83b219f0c51ac2872618fec9d56
Alexrupa27 <alexrupa123@gmail.com>
2024-11-19 17:56:48 +0100

    Music, Icon, Fix Button

--- SCRIPT/control.gd
+++ SCRIPT/control.gd
@@ -4,8 +4,9 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://ESCENAS/niveles.tscn")
 
 
+
 func _on_instruccions_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://ESCENAS/intruccions.tscn")
 
 
 func _on_exit_pressed() -> void:
